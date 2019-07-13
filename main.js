7b375752110a3225165f3bb37f5e2d224afdd3a1
Your Name <you@example.com>
2019-07-13 03:31:48 -0700

    update

---
+++ main.js
@@ -0,0 +1,7 @@
+console.log('')
+
+alert('scratch cat has risen')
+alert('all hail the lord and savior')
+
+
+let response = prompt('are you ready to religiously devote yourself to the scratch lord?')
