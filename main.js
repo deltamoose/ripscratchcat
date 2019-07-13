0b308ea265470973a08158744a8d78d3f91d0bea
deltamoose <improvidentia@gmail.com>
2019-07-12 23:28:14 -0700

    color update

--- main.js
+++ main.js
@@ -1,5 +1,7 @@
-alert("Your computer has ")
+alert("Your computer has been infected by a virus. Call 818-279-4723 for technical assistance.")
 
 alert("SCRATCH CAT IS GONE !! ")
+
+document.getElementById("red")
 
 // document.getElementById("Rip").innerHTML
