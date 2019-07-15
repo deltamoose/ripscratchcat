ef6015dc1ceaf44d59d4c363d057cb3cfa016c32
deltamoose <50969243+deltamoose@users.noreply.github.com>
2019-07-15 11:18:54 -0700

    Update main.js

--- main.js
+++ main.js
@@ -1,10 +1,10 @@
 console.log('')
 
-alert('scratch cat has risen')
-alert('all hail the lord and savior')
+// alert('scratch cat has risen')
+// alert('all hail the lord and savior')
 
 
-let response = prompt('are you ready to religiously devote yourself to the scratch lord?')
+// let response = prompt('are you ready to religiously devote yourself to the scratch lord?')
 alert("Your computer has been infected by a virus. Call 818-279-4723 for technical assistance.")
 
 alert("SCRATCH CAT IS GONE !! ")
